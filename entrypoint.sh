02173136bd73b0e9edf3ef39aaf1bf844fe2570b
Munir Azme <munirazme@live.com>
2024-01-16 19:07:11 +1100

    Removed print from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,4 @@ source "/catkin_ws/rover_sim_ws/install/setup.bash" && \
 # exec "rviz2" 
 # echo Please enter goal pose in the following format: x y z w
 IFS=' ' read -ra goal_pose -p "Please enter goal pose in the following format: x y z w : "
-echo "last item is: ${goal_pose[2]}"
 ros2 launch rover_gz lunar_sim.launch.py & ros2 topic pub /goal_pose geometry_msgs/PoseStamped "{header: {stamp: {sec: 0}, frame_id: 'map'}, pose: {position: {x: ${goal_pose[0]}, y: ${goal_pose[1]}, z: ${goal_pose[2]}}, orientation: {w: ${goal_pose[3]}}}}"
